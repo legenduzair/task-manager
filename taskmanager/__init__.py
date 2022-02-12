c0646977db93c154beafb96a6a0540d237ef0cc1
legenduzair <uzairoomer@live.com>
2022-02-12 15:00:19 +0000

    Delete commented code

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")  # heroku
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
